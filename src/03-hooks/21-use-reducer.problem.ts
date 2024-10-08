eaad9a0c05ed7981f8d0c1d4723821b9e5ac0d1e
mtthdz <matt@mtthdz.com>
2024-10-08 18:23:38 -0400

    reducer types

--- src/03-hooks/21-use-reducer.problem.ts
+++ src/03-hooks/21-use-reducer.problem.ts
@@ -1,7 +1,26 @@
 import { useReducer } from "react";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const reducer = (state: unknown, action: unknown) => {
+const reducer = (
+  state: {
+    count: number;
+  },
+    /**
+     * this should work: 
+     *  type: 'add' | 'subtract';
+     *  add?: number;
+     *  subtract?: number;
+     * 
+     * but because third error, you'll notice we pass 'add' but no subsequent val. 
+     * We'll need to rely on unions to handle chained cases like this
+     */
+  action: {
+    type: 'add';
+    add: number;
+  } | {
+    type: 'subtract';
+    subtract: number;
+  }) => {
   switch (action.type) {
     case "add":
       return { count: state.count + action.add };
